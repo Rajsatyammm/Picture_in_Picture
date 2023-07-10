e8924432ea2af7c7af49b5b14411f23cde3e40a2
Satyam Raj <rajsatyammm@gmail.com>
2023-07-10 23:09:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const videoElement = document.querySelector('#video');
 const btn = document.querySelector('.button-container');
-
-
+const singleClick = document.querySelector('#singleClick');
+const dblClick = document.querySelector('#dblClick');
 
 async function selectMediaStream() {
     try {
@@ -9,6 +9,9 @@ async function selectMediaStream() {
         videoElement.srcObject = mediaStream;
         videoElement.onloadedmetadata = () => {
             videoElement.play();
+            dblClick.classList.add('hide');
+            singleClick.classList.add('active');
+            singleClick.classList.remove('hide');
         }
     } catch(e) {
         console.log('Error :', e);
@@ -18,6 +21,8 @@ async function selectMediaStream() {
 async function handlePictureInPicture() {
     // console.log('click hua bro');
     await videoElement.requestPictureInPicture();
+    singleClick.classList.add('hide');
+
     // btn.classList.add('hide');
 }
 
